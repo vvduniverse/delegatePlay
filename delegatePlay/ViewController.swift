598dfd09544e786615245da345a1bd46a09ef11b
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-09-17 14:00:56 +0300

    Initial Commit

---
+++ delegatePlay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  delegatePlay
+//
+//  Created by Vahtee Boo on 17.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
