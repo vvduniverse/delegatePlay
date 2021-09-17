4c387015dfbbbd676e98d64eef9c6a56ed6ee849
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-09-17 14:43:53 +0300

    UI elements add connections

--- delegatePlay/MainViewController.swift
+++ delegatePlay/MainViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
+protocol SetTextFieldsDelegate {
+    func setTextFields(name: String, surname: String)
+}
+
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var surnameLabel: UILabel!
+    
+    @IBOutlet weak var fullnameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let settingsVC = segue.destination as! SettingsViewController
+        settingsVC.delegate = self
+        settingsVC.name = nameLabel.text
+        settingsVC.surname = surnameLabel.text
+    }
 
 
+}
+
+extension MainViewController: SetTextFieldsDelegate {
+    func setTextFields(name: String, surname: String) {
+        nameLabel.text = name
+        surnameLabel.text = surname
+        fullnameLabel.text = name + surname
+    }
+    
+    
 }
 
--- delegatePlay/SettingsViewController.swift
+++ delegatePlay/SettingsViewController.swift
@@ -8,22 +8,24 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    
+    var name: String!
+    var surname: String!
+    
+    var delegate: SetTextFieldsDelegate!
 
+    @IBOutlet weak var nameTF: UITextField!
+    @IBOutlet weak var surnameTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func doneButtonPressed() {
     }
-    */
+    
+    
 
 }
